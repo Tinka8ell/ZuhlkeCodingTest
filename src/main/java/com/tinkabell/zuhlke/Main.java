d1b341e557df558a52df5af0531e33fb6f272839
Tinka8ell <mark.ingamells@gmail.com>
2022-08-13 08:52:56 +0100

    Modify project structure

---
+++ src/main/java/com/tinkabell/zuhlke/Main.java
@@ -0,0 +1,13 @@
+package com.tinkabell.zuhlke;
+
+public class Main {
+
+    public String helloWorld() {
+        return ""; // temporary make fail! "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.helloWorld());
+    }
+}
